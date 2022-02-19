55d1b4545f80dd2adb23fdcb92ca83a505ac531d
rizkytm <ritrime97@gmail.com>
2022-02-19 21:39:54 +0700

    install graphql and apollo client

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import Detail from './pages/Detail';
 import MyPokemon from './pages/MyPokemon';
 import Navbar from './components/Navbar';
 import TopNavbar from './components/TopNavbar';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://beta.pokeapi.org/graphql/v1beta',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <Router>
-      <TopNavbar />
-      <div className="container p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/my-pokemon" element={<MyPokemon />} />
-        </Routes>
-      </div>
-      <Navbar />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <TopNavbar />
+        <div className="container p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/my-pokemon" element={<MyPokemon />} />
+          </Routes>
+        </div>
+        <Navbar />
+      </Router>
+    </ApolloProvider>
   );
 }
 
